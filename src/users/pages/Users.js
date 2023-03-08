2d06a2ec64f8cc1579e1d217795202b3fed8f36e
UgurcanOruc <ugurcan-oruc@hotmail.com>
2023-03-08 18:49:19 +0300

    Header, user card and hamburger menu is created

--- src/users/pages/Users.js
+++ src/users/pages/Users.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import UserList from '../components/UsersList';
 
 const Users = () => {
-    return <h2>Users Works!</h2>;
+    const USERS = [
+      {
+        id: "u1",
+        name: "Ugurcan",
+        image:
+          "https://iasbh.tmgrup.com.tr/8f9cae/650/344/0/12/1280/684?u=https://isbh.tmgrup.com.tr/sbh/2022/12/13/avatar-filmi-1670932751007.jpg",
+        places: 3,
+      },
+    ];
+
+    return <UserList items={USERS} />;
 };
 
 export default Users;
